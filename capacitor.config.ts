432a69c4785f04ec3b2918cb44888a52a3c069c2
Charlie Kaiman <ckaiman@charlies-mpb-3.fios-router.home>
2022-11-16 09:00:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hffi-mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
